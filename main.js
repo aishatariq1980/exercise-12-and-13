93c4fcc0408d48e8bb522f34d83e8d4e881adad7
aishatariq1980 <167800892+aishatariq1980@users.noreply.github.com>
2024-04-22 22:48:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+let taransportionmodes = ["bus", "rikshaw", "motoerbikes", "cars"];
+taransportionmodes.forEach(mode => console.log(`i would like to own a ${mode} .`));
---
+++ main.ts
@@ -0,0 +1,3 @@
+let taransportionmodes = ["bus" , "rikshaw" , "motoerbikes" ,"cars"] ;
+
+taransportionmodes . forEach(mode => console.log(`i would like to own a ${mode} .`)) ;
